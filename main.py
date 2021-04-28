98dfc450319b9c2c94a9f546ba698590a27154ed
bautrd1 <81129560+bautrd1@users.noreply.github.com>
2021-04-28 20:28:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from pprint import pprint
+
+token = "MDhmMDRmMDUtOGE4YS00MjY3LWE1MDEtYjFiMmRjMTA5NDM2NWY0ZjEwZDMtNDVh_P0A1_ebee85b6-002e-44c5-8392-ccef6152c511"
+BASE_URL = "https://webexapis.com"
+room = "v1/rooms"
+
+body = {'title': "Sheldons Room"}
+headers = {
+    'Authorization': f"Bearer {token}",
+    'Content-Type': "application/json"
+}
+
+room_url = f"{BASE_URL}/{room}"
+resp = requests.get(room_url, headers=headers, data=json.dumps(body))
+pprint(resp.json(), indent=4)
